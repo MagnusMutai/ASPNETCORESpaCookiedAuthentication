5942d7aa7b9cbadceaeff8cc7e50b4a737ec3292
Magnus Mutai <MagnusMq07@gmail.com>
2024-04-27 12:11:45 +0300

    Great progress

--- c.SsrFramework/Program.cs
+++ c.SsrFramework/Program.cs
@@ -1,6 +1,41 @@
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthentication("def")
+    .AddCookie("def");
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddCors(o => {
+    o.AddPolicy("fe", pb => pb.WithOrigins("http://localhost:3000").AllowCredentials().AllowAnyMethod().AllowAnyHeader());
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors("fe");
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/api/test", () => "secret!").RequireAuthorization();
+
+app.MapPost("/api/login", async ctx =>
+{
+    await ctx.SignInAsync("def", new ClaimsPrincipal(
+
+        new ClaimsIdentity(
+            new Claim[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+            },
+            "def"
+        )
+      ),
+      new AuthenticationProperties()
+      {
+          IsPersistent = true
+      });
+});
 
 app.Run();
